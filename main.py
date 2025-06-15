ccf138afb2b67dbe3e32bcdb889520d366c9680c
dta0one <github@davidtao.net>
2025-06-14 22:04:37 -0700

    AIA CH2L1a

--- main.py
+++ main.py
@@ -6,20 +6,23 @@ from google.genai import types
 def main():
     load_dotenv()
 
-    if len(sys.argv) < 2:
-        print ("No prompt provided")
+    args = sys.argv[1:]
+
+    if not args:
+        print ("Error - no prompt provided")
+        print ('Usage: python main.py "prompt1" "prompt2" "etc..."')
+        print ('For verbose response, add --verbose or --v at the end')
+        print ('Usage: python main.py "prompt1" "prompt2" --verbose')
         sys.exit(1)    
 
-    user_prompts = sys.argv[1:]
+    user_prompts = " ".join(args)
     ai_model = "gemini-2.0-flash-001"
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompts)]),
     ]
-
-    api_key = os.environ.get("GEMINI_API_KEY")
-
-    client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model=ai_model,
@@ -29,10 +32,15 @@ def main():
     prompt_tokens_used = response.usage_metadata.prompt_token_count
     response_tokens_used = response.usage_metadata.candidates_token_count
 
-    print()
-    print(f"Prompt tokens: {prompt_tokens_used}")
-    print(f"Response tokens: {response_tokens_used}")
-    print()
+    if "--verbose" in args or "--v" in args:
+        print()
+        print("Verbose Mode Enabled")
+        print(f"User prompt: {user_prompts}")
+        print(f"Prompt tokens: {prompt_tokens_used}")
+        print(f"Response tokens: {response_tokens_used}")
+        print()
+
+
     print("Response:")
     print(response.text)
     print()
