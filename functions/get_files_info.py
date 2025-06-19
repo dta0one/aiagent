d4480e1881a74c2b04c5eeda31eade702a4202c0
dta0one <github@davidtao.net>
2025-06-19 09:13:52 -0700

    AIA CH2L2e

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,6 +3,10 @@ import os
 def get_files_info(working_directory, directory=None):
     if directory is None:
         directory = working_directory
+    
+    directory = os.path.join(working_directory, directory)
+    directory = os.path.abspath(directory)
+    working_directory = os.path.abspath(working_directory)
 
     if is_outside_working_directory(working_directory, directory):
         return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
@@ -10,8 +14,6 @@ def get_files_info(working_directory, directory=None):
     if not os.path.isdir(directory):
         return (f'Error: "{directory}" is not a directory')
 
-
-#####
     try:
         # Get the list of all files and directories in the specified path
         entries = os.listdir(directory)
@@ -35,11 +37,8 @@ def get_files_info(working_directory, directory=None):
 
     except FileNotFoundError:
         return f'Error: "{directory}" is not a directory'
-    #####
 
 
 def is_outside_working_directory(working_directory, directory):
-    directory = os.path.join(working_directory, directory)
-    directory = os.path.abspath(directory)
-    working_directory = os.path.abspath(working_directory)
+
     return not directory.startswith(working_directory)
